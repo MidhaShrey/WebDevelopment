3c92c01d2143c588e383549c1a45d96651929284
MidhaShrey <midhashrey@gmail.com>
2022-07-15 17:27:53 +0530

    Working with DOM

--- JavaScript/tut11.js
+++ JavaScript/tut11.js
@@ -1,2 +1,15 @@
 // DOM => Document Object Model
-console.log("This is tutorial 11");
+console.log('This is tut 11');
+let a = window.document;
+alert('hello Shrey');
+a = prompt('This will destroy your computer. Type your name');
+
+a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight;
+a = window.stop;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+a = location.toString();
+a = window.history;
+console.log(a);          
