902e997027f8a13912a4f2273ae43255f0794c62
MidhaShrey <midhashrey@gmail.com>
2022-07-24 00:38:36 +0530

    JavaScript updates to tutorial 13

---
+++ JavaScript/tut12.js
@@ -0,0 +1,26 @@
+console.log("Welcome to Tutorial 12");
+
+// let a = document;
+// a = document.all;
+// a = document.body;
+// a = document.forms[0];
+
+let a = document.all;
+
+a = document.href;
+// Array.from(a).forEach(function(element)){
+// 	console.log(element)
+// }
+
+// Assignment
+// Use document.images and document.scripts and print the list of images and scripts on html page
+
+let b = document.images;
+
+a = document.scripts;
+
+console.log(a);
+
+console.log(b);
+
+// DOM has a tree Structure
---
+++ JavaScript/tut14.js
@@ -0,0 +1,4 @@
+console.log("This is Tutorial");
+
+
+// DOM Selectors
